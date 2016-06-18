8c7f7a6272a9664d81ce2a13cfeea81c76cb41b8
Shun Takahashi <shun.takahashi0728@gmail.com>
2016-06-18 11:10:45 +0900

    locationとchatのイベントを分割。その他諸々修正

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 var milkcocoa = new MilkCocoa("yourOwnAppName.mlkcca.com");
+var locationDataStore = milkcocoa.dataStore("location");
 var chatDataStore = milkcocoa.dataStore("chat");
-var textArea, board;
-var chatMessage = "";
+var textArea, board, chatMessage;
 
 window.onload = function(){
     textArea = document.getElementById("msg");
@@ -21,7 +21,7 @@ window.onload = function(){
     setInterval(function(){
         GMaps.geolocate({
           success: function(position) {
-            chatDataStore.send({
+            locationDataStore.send({
                 lat : position.coords.latitude,
                 lng : position.coords.longitude,
             });
@@ -33,9 +33,9 @@ window.onload = function(){
             console.log("geolocate not support");
           },
         });
-    },10000);//10秒ごとに位置情報送信
+    },5000);//5秒ごとに位置情報送信
 
-    chatDataStore.on('send', function(data) {
+    locationDataStore.on('send', function(data) {
         var lat = data.value.lat, lng = data.value.lng;
         map.setCenter(lat, lng);
         map.drawOverlay({
@@ -52,6 +52,41 @@ window.onload = function(){
 
 function clickEvent(){
   var chatMessage = textArea.value;
-  $('#chatMessage').text(chatMessage);
   textArea.value = "";
-}
+  chatDataStore.push(
+    { 
+      message : chatMessage,
+      date    : dateFormatYYYYMMDDHHNNSS(new Date())
+    },
+    function(err, pushed){
+      // console.log("chatMessage pushed");
+    },
+    function(err) {
+      console.log("chatMessage push failed "+err);
+    }
+  )
+};
+
+$(function() {
+  chatDataStore.on("push", function(e) {
+    console.log(JSON.stringify(e));
+    console.log(e.value.message);
+    $('#chatMessage').text(e.value.message);
+  });
+
+  dateFormatYYYYMMDDHHNNSS = function(date){
+    var YYYY = date.getYear();
+    if (YYYY < 1900){YYYY += 1900}
+    var MM = String(date.getMonth()+1);
+    if (MM.length < 2){MM = "0" + MM}
+    var DD = String(date.getDate());
+    if (DD.length < 2){DD = "0" + DD}
+    var HH = String(date.getHours());
+    if (HH.length < 2){HH = "0" + HH}
+    var NN = String(date.getMinutes());
+    if (NN.length < 2){NN = "0" + NN}
+    var SS = String(date.getSeconds());
+    if (SS.length < 2){SS = "0" + SS}
+    return Number(String(YYYY) + MM + DD + HH + NN + SS);
+  }
+});
